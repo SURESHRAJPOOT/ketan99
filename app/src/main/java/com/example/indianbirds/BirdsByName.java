3e7e8aee490c0110e30bdcfb1b8c1badab9d95a6
SURESHRAJPOOT <67622794+SURESHRAJPOOT@users.noreply.github.com>
2023-03-06 15:48:26 +0530

    111

---
+++ app/src/main/java/com/example/indianbirds/BirdsByName.java
@@ -0,0 +1,26 @@
+package com.example.indianbirds;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BirdsByName extends Fragment {
+
+
+    public BirdsByName() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_birds_by_name, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/indianbirds/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.indianbirds;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    ImageView rImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView= findViewById(R.id.navigationView);
+        toolbar= findViewById(R.id.toolbar);
+
+
+        // getting ImageView by its id
+        rImage = findViewById(R.id.rImage);
+
+        // we will get the default FirebaseDatabase instance
+        FirebaseDatabase firebaseDatabase
+                = FirebaseDatabase.getInstance();
+
+        // we will get a DatabaseReference for the database
+        // root node
+        DatabaseReference databaseReference
+                = firebaseDatabase.getReference();
+
+        // Here "image" is the child node value we are
+        // getting child node data in the getImage variable
+        DatabaseReference getImage = databaseReference.child("image");
+
+        // Adding listener for a single change
+        // in the data at this location.
+        // this listener will triggered once
+        // with the value of the data at the location
+        getImage.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(
+                            @NonNull DataSnapshot dataSnapshot)
+                    {
+                        // getting a DataSnapshot for the
+                        // location at the specified relative
+                        // path and getting in the link variable
+                        String link = dataSnapshot.getValue(
+                                String.class);
+
+                        // loading that data into rImage
+                        // variable which is ImageView
+                        Picasso.get().load(link).into(rImage);
+                    }
+
+                    // this will called when any problem
+                    // occurs in getting data
+                    @Override
+                    public void onCancelled(
+                            @NonNull DatabaseError databaseError)
+                    {
+                        // we are showing that error message in
+                        // toast
+                        Toast
+                                .makeText(MainActivity.this,
+                                        "Error Loading Image",
+                                        Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+
+       //step 1
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        loadFragment(new Home());
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id==R.id.home){
+                    loadFragment( new Home());
+                } else if (id==R.id.birds_by_species) {
+                    loadFragment( new BirdsBySpecies());
+                } else if (id==R.id.birds_by_indian_name) {
+                    loadFragment( new BirdsByName());
+                } else if (id==R.id.explore) {
+                    loadFragment( new Explore());
+                } else if (id==R.id.location) {
+                    loadFragment( new Location());
+                } else if (id==R.id.aboutus) {
+                    loadFragment( new AboutUs());
+                }else {
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragment);
+        ft.commit();
+    }
+}
